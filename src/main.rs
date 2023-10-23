2e2902b6975c42dc535f9b356124a4761cb75b94
Alex Gorichev <Alex.Gorichev@protonmail.com>
2023-10-23 10:29:38 +0100

    Refactor

--- src/main.rs
+++ src/main.rs
@@ -1,64 +1,60 @@
 use egui::Ui;
+use egui_plot::{Line, PlotPoints};
 
 use eframe::egui;
 
 fn main() {
     let native_options = eframe::NativeOptions::default();
-    eframe::run_native(
+    let res = eframe::run_native(
         "Mitrella",
         native_options,
-        Box::new(|cc| Box::new(MyEguiApp::new(cc))),
+        Box::new(|cc| Box::new(MitrellaApp::new(cc))),
     );
+    if let Err(e) = res {
+        eprintln!("{e}");
+    }
 }
 
 #[derive(Default)]
-struct MyEguiApp {}
+struct MitrellaApp {}
 
-impl MyEguiApp {
+impl MitrellaApp {
     fn new(_cc: &eframe::CreationContext<'_>) -> Self {
         // Customize egui here with cc.egui_ctx.set_fonts and cc.egui_ctx.set_visuals.
         // Restore app state using cc.storage (requires the "persistence" feature).
-        // Use the cc.gl (a glow::Context) to create graphics shaders and buffers that you can use
-        // for e.g. egui::PaintCallback.
         Self::default()
     }
 }
 
-impl eframe::App for MyEguiApp {
+impl eframe::App for MitrellaApp {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
         egui::CentralPanel::default().show(&ctx, |ui| {
-            plot(ui);
+            let equations = vec![|x: f64| x.sin(), |x: f64| x.cos()];
+            plot(ui, equations);
         });
     }
 }
 
-fn plot(ui: &mut Ui) {
-    use egui_plot::{Line, PlotPoints};
-    let n = 128;
-    let sin = Line::new(
-        (0..=n)
-            .map(|i| {
-                use std::f64::consts::TAU;
-                let x = egui::remap(i as f64, 0.0..=n as f64, -TAU..=TAU);
-                [x, x.sin()]
-            })
-            .collect::<PlotPoints>(),
-    );
-    let cos = Line::new(
-        (0..=n)
-            .map(|i| {
-                use std::f64::consts::TAU;
-                let x = egui::remap(i as f64, 0.0..=n as f64, -TAU..=TAU);
-                [x, x.cos()]
-            })
-            .collect::<PlotPoints>(),
-    );
-    egui_plot::Plot::new("example_plot")
+fn plot(ui: &mut Ui, equations: Vec<impl Fn(f64) -> f64>) {
+    let lines = equations.into_iter().map(to_line);
+    egui_plot::Plot::new("main_plot")
         .show_axes(true)
         .data_aspect(1.0)
         .show(ui, |plot_ui| {
-            plot_ui.line(sin);
-            plot_ui.line(cos)
+            for line in lines {
+                plot_ui.line(line);
+            }
         })
         .response;
+}
+
+fn to_line(equation: impl Fn(f64) -> f64) -> Line {
+    let total_points = 128;
+    let plot_point = |n| {
+        use std::f64::consts::TAU;
+        let x = egui::remap(n as f64, 0.0..=total_points as f64, -TAU..=TAU);
+        [x, equation(x)]
+    };
+    let points: PlotPoints = (0..=total_points).map(plot_point).collect();
+    Line::new(points)
 }
