3140f5e6e9c1bd3dc2aca543b3b358ab7db5f940
Alex Gorichev <Alex.Gorichev@protonmail.com>
2023-10-23 10:45:57 +0100

    Further refactoring

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use egui::Ui;
-use egui_plot::{Line, PlotPoints};
+use egui_plot::{Line, PlotPoints, PlotUi};
+use std::f64::consts::TAU;
 
 use eframe::egui;
 
@@ -37,24 +38,26 @@ impl eframe::App for MitrellaApp {
 
 fn plot(ui: &mut Ui, equations: Vec<impl Fn(f64) -> f64>) {
     let lines = equations.into_iter().map(to_line);
+    let build_fn = |ui: &mut PlotUi| build_plot(ui, lines);
     egui_plot::Plot::new("main_plot")
         .show_axes(true)
         .data_aspect(1.0)
-        .show(ui, |plot_ui| {
-            for line in lines {
-                plot_ui.line(line);
-            }
-        })
+        .show(ui, build_fn)
         .response;
 }
 
 fn to_line(equation: impl Fn(f64) -> f64) -> Line {
     let total_points = 128;
     let plot_point = |n| {
-        use std::f64::consts::TAU;
         let x = egui::remap(n as f64, 0.0..=total_points as f64, -TAU..=TAU);
         [x, equation(x)]
     };
     let points: PlotPoints = (0..=total_points).map(plot_point).collect();
     Line::new(points)
+}
+
+fn build_plot(plot_ui: &mut PlotUi, lines: impl Iterator<Item = Line>) {
+    for line in lines {
+        plot_ui.line(line);
+    }
 }
