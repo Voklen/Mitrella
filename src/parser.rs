e1c34a416fa5257ba463d9a5894946283418f5b7
Alex Gorichev <Alex.Gorichev@protonmail.com>
2023-10-29 12:17:02 +0000

    Rewrite to only parse once (not every point)

--- src/parser.rs
+++ src/parser.rs
@@ -1,54 +1,79 @@
-struct Stack<T> {
-    stack: Vec<T>,
+struct Stack {
+    stack: Vec<f64>,
 }
 
-impl<T> Stack<T> {
+impl Stack {
     pub fn new() -> Self {
         return Self { stack: Vec::new() };
     }
 
-    pub fn push(&mut self, value: T) {
+    pub fn push(&mut self, value: f64) {
         self.stack.push(value)
     }
 
-    pub fn pop(&mut self) -> Option<T> {
+    pub fn pop(&mut self) -> Option<f64> {
         self.stack.pop()
     }
 }
 
+type StackFunc = Box<dyn Fn(f64, &mut Stack) -> Option<()>>;
+
+macro_rules! closure {
+    ($input: expr) => {
+        Some(Box::new($input))
+    };
+}
+
 pub fn parse_equation(equation: &str) -> Box<dyn Fn(f64) -> Option<f64>> {
     let actual = equation.to_owned();
-    let func = move |x: f64| {
-        let tokens = actual.split_whitespace();
-        let mut stack = Stack::new();
-        for token in tokens.clone() {
-            if let Ok(num) = token.parse::<i64>() {
-                stack.push(num as f64);
-            };
-            match token {
-                "+" => {
-                    let sum = stack.pop()? + stack.pop()?;
-                    stack.push(sum);
-                }
-                "-" => {
-                    let not_sum = stack.pop()? - stack.pop()?;
-                    stack.push(not_sum);
-                }
-                "*" => {
-                    let product = stack.pop()? * stack.pop()?;
-                    stack.push(product);
-                }
-                "/" => {
-                    let quotient = stack.pop()? / stack.pop()?;
-                    stack.push(quotient);
-                }
-                "x" => stack.push(x),
-                _ => return None,
-            }
-        }
-        return stack.pop();
+    let tokens = actual.split_whitespace();
+    let funcs: Vec<StackFunc> = tokens.map(to_funcs).flatten().collect();
+    let res = move |x: f64| run_stack_funcs(x, &funcs);
+    Box::new(res)
+}
+
+fn run_stack_funcs(x: f64, funcs: &Vec<StackFunc>) -> Option<f64> {
+    let mut stack = Stack::new();
+    for func in funcs {
+        func(x, &mut stack);
+    }
+    return stack.pop();
+}
+
+fn to_funcs(token: &str) -> Option<StackFunc> {
+    if let Ok(num) = token.parse::<i64>() {
+        return closure!(move |_x, stack: &mut Stack| {
+            stack.push(num as f64);
+            Some(())
+        });
     };
-    Box::new(func)
+    match token {
+        "+" => closure!(|_x, stack: &mut Stack| {
+            let sum = stack.pop()? + stack.pop()?;
+            stack.push(sum);
+            Some(())
+        }),
+        "-" => closure!(|_x, stack: &mut Stack| {
+            let not_sum = stack.pop()? - stack.pop()?;
+            stack.push(not_sum);
+            Some(())
+        }),
+        "*" => closure!(|_x, stack: &mut Stack| {
+            let product = stack.pop()? * stack.pop()?;
+            stack.push(product);
+            Some(())
+        }),
+        "/" => closure!(|_x, stack: &mut Stack| {
+            let quotient = stack.pop()? / stack.pop()?;
+            stack.push(quotient);
+            Some(())
+        }),
+        "x" => closure!(|x: f64, stack: &mut Stack| {
+            stack.push(x);
+            Some(())
+        }),
+        _ => return None,
+    }
 }
 
 #[test]
